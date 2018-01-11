f869a53fbb54003bedd9c8fd6d332941a54321a3
Bryan <kenshin2004528@gmail.com>
2018-01-11 17:28:28 +0800

    updated

--- ksql/__init__.py
+++ ksql/__init__.py
@@ -1,7 +1,7 @@
 
 __package_name__ = "ksql"
 __ksql_server_version__ = "0.3"
-__ksql_api_version__ = "0.2.0"
+__ksql_api_version__ = "0.1.1"
 __version__ = __ksql_server_version__ + "." + __ksql_api_version__
 
 from ksql.client import KSQLAPI
